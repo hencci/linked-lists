9a1a7e06246cb412fb8af0383e9e61a7cafcf82c
hencci <hencci20@gmail.com>
2025-05-29 13:16:44 +0100

    Create a class to represent a single element

---
+++ src/node.js
@@ -0,0 +1,7 @@
+// Node class representing a single element in the linked list
+export class Node {
+    constructor(value = null) {
+        this.value = value;
+        this.nextNode = null;
+    }
+}
